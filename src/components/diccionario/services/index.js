2e1d8f49fe05a01ce296caa52510c21c9a0044f5
Rodolfo Diaz <rodolfodiaz.005@gmail.com>
2022-05-28 23:14:41 -0400

    Se cambia URL Api Somee

--- src/components/diccionario/services/index.js
+++ src/components/diccionario/services/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const baseURL = process.env.REACT_APP_BASE_URL
-
+//const baseURL = process.env.REACT_APP_BASE_URL
+const baseURL = "http://www.diccionarioprogramacion.somee.com/api";
 
 export async function getDefiniciones(){
         try{
